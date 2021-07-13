c5d681448768d2452222d767bdf318082c25df91
jing Wang <admin@jingwang.com>
2021-07-13 19:43:44 +0800

    Create vote app and mapping root url, home_page view return minimal html.

---
+++ vote/tests.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+from vote.views import home_page
+
+# Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_home_page_can_resolve_root_url(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_can_return_correct_content(self):
+        request = HttpRequest()
+        response = home_page(request)
+
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Voting For Your Question</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
+
+
+
