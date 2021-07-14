1bb610291834cb96225196eaa7cb7d2d3416b155
jing Wang <admin@jingwang.com>
2021-07-14 10:09:29 +0800

    Upgrade functional test user story.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -8,6 +8,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
+import time
+
+MAX_TIME = 3
+QUESTION_PLACEHOLDER = 'Submit a question to voting!'
+VOTE_PLACEHOLDER = 'Submit a vote for the question!'
+FIRST_QUESTION = 'Where is the restarant for dinner?'
+FIRST_VOTE = 'McDonald\'s is the best choice.'
+SECOND_VOTE = 'I\'d like to KFC.'
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -20,6 +28,36 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def wait_to_check_text_in_the_table(self, text, table_id):
+        START_TIME = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, table_id)
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(
+                    text, 
+                    [row.text for row in rows]
+                )
+                return
+            except (AssertionError, WebdriverException) as e:
+                if time.time() - START_TIME > MAX_TIME:
+                    raise e
+                time.sleep(0.5)
+
+    def wait_to_check_text_in_the_page(self, text):
+        START_TIME = time.time()
+        while True:
+            try:
+                html = self.browser.find_element(By.TAG_NAME, 'body').text
+                self.assertIn(text, html)
+                return
+            except (AssertionError, WebdriverException) as e:
+                if time.time() - START_TIME > MAX_TIME:
+                    raise e
+                time.sleep(0.5)
+        
+
+
     def test_submit_a_question_and_votes_it_twice(self):
         # Joe visits the website
         self.browser.get(self.live_server_url)
@@ -29,18 +67,50 @@ class NewVisitorTest(LiveServerTestCase):
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('Voting For Your Question', header_text)
         # there is inputbox to invite him to submit a question
+        inputbox = self.browser.find_element(By.ID, 'new-question')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            QUESTION_PLACEHOLDER
+        )
         # Joe submit a question
+        inputbox.send_keys(FIRST_QUESTION)
+        inputbox.send_keys(keys.ENTER)
         # Joe see the question, 
+        self.wait_to_check_text_in_the_page(FIRST_QUESTION)
         # and he see a new inputbox to invite him to submit a vote
+        inputbox = self.browser.find_element(By.ID, 'new-vote')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            VOTE_PLACEHOLDER
+            
+        )
         # Joe submit a vote agian
+        inputbox.send_keys(FIRST_VOTE)
+        inputbox.send_keys(keys.ENTER)
         # Joe see the question and vote
+        self.wait_to_check_text_in_the_table('1. '+FIRST_VOTEE, 'votes-table')
+        self.wait_to_check_text_in_the_page(FIRST_QUESTION)
         # He feels good and leave
+        self.browser.quit()
+        self.setUp()
         # Kim visit the website,
+        self.browser.get(self.live_server_url)
         # Kim see the question of Joe, he click the question 
-        # There is a inputbox to invite him to submit a vote
-        # Kim vote 
+        self.wait_to_check_text_in_the_table('1. '+FIRST_QUESTION, 'questions-table')
+        FIRST_QUESTION_LINK = self.browser.find_element(By.PARTIAL_LINK_TEXT, FIRST_QUESTION)
+        FIRST_QUESTION_LINK.click()
+        time.sleep(0.3)
+        # Kim see the question and vote of Joe
+        self.wait_to_check_text_in_the_page(FIRST_QUESTION)
+        inputbox = self.browser.find_element(By.ID, 'new-vote')
+        # Kim vote agian 
+        inputbox.send_keys(FIRST_VOTE)
+        inputbox.send_keys(keys.ENTER)
         # Kim see the question and vote
+        self.wait_to_check_text_in_the_table('2. '+SECOND_VOTEE, 'votes-table')
+        self.wait_to_check_text_in_the_page(FIRST_QUESTION)
         # kim see the vote of joe
+        self.wait_to_check_text_in_the_table('1. '+FIRST_VOTEE, 'votes-table')
         
 
 
