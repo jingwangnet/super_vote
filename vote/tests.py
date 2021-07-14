d5acc92b6555a6df476df1587d7f9670f6968cee
jing Wang <admin@jingwang.com>
2021-07-14 09:35:36 +0800

    Refactory home_page unit test used template

--- vote/tests.py
+++ vote/tests.py
@@ -6,19 +6,14 @@ from vote.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
 
-    def test_home_page_can_resolve_root_url(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_home_page_used_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'vote/index.html')
 
-    def test_home_page_can_return_correct_content(self):
-        request = HttpRequest()
-        response = home_page(request)
 
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>Voting For Your Question</title>', html)
-        #self.fail(repr(html))
-        self.assertTrue(html.strip().endswith('</html>'))
+        
+
+
 
 
 
